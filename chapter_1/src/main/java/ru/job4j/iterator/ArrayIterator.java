d2d1f662f619c6748cedae65c9b02605027781a7
Sergey Baikov <rayleonard9@gmail.com>
2019-03-07 10:21:55 +0300

    Итератор для двухмерного массива int[][][#118876]

--- chapter_1/src/main/java/ru/job4j/iterator/ArrayIterator.java
+++ chapter_1/src/main/java/ru/job4j/iterator/ArrayIterator.java
@@ -2,18 +2,57 @@ package ru.job4j.iterator;
 
 import java.util.Iterator;
 
+/**
+ * @author Sergey Baikov
+ * @version $ 1 $
+ * @since 07.03.19
+ */
 public class ArrayIterator implements Iterator<Integer> {
 
+    /**
+     * Хранит массив
+     */
+    private final int[][] values;
+    /**
+     * Хранит индекс строик массива
+     */
+    private int indexI = 0;
+    /**
+     * Хранит индекс столбца массива
+     */
+    private int indexJ = 0;
+
+    /**
+     * Конструктор
+     * @param ints входящий массив
+     */
     public ArrayIterator(int[][] ints) {
+        this.values = ints;
     }
 
+    /**
+     * Метод проверяет наличие следующего элемента в мсассиве
+     * @return есть элемент / нет элемента
+     */
     @Override
     public boolean hasNext() {
-        return false;
+        return indexI < values.length;
     }
 
+    /**
+     * Метод возвращает текущий элемент массива и сдвигает каретку (указатель) на следующий элемент
+     * @return элемент массива
+     */
     @Override
     public Integer next() {
-        return null;
+        Integer result = values[indexI][indexJ];
+        if (hasNext()) {
+            indexJ++;
+            if (indexJ == values[indexI].length) {
+                indexJ = 0;
+                indexI++;
+            }
+        }
+        return result;
     }
 }
--- chapter_1/src/test/java/ru/job4j/iterator/ArrayIteratorTest.java
+++ chapter_1/src/test/java/ru/job4j/iterator/ArrayIteratorTest.java
@@ -6,26 +6,36 @@ import java.util.Iterator;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-
+/**
+ * @author Sergey Baikov
+ * @version $ 1 $
+ * @since 07.03.19
+ */
 public class ArrayIteratorTest {
 
     private Iterator<Integer> it;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         it = new ArrayIterator(new int[][]{{1}, {3, 4}, {7}});
     }
 
+    /**
+     * Test next
+     */
     @Test
-    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation () {
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
         assertThat(it.next(), is(1));
         assertThat(it.next(), is(3));
         assertThat(it.next(), is(4));
         assertThat(it.next(), is(7));
     }
 
+    /**
+     * Test hasNext and next
+     */
     @Test
-    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder () {
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
         assertThat(it.hasNext(), is(true));
         assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(1));
@@ -33,9 +43,11 @@ public class ArrayIteratorTest {
         assertThat(it.next(), is(4));
         assertThat(it.next(), is(7));
     }
-
+    /**
+     * Test hasNext and next
+     */
     @Test
-    public void hasNextNextSequentialInvocation () {
+    public void hasNextNextSequentialInvocation() {
         assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(1));
         assertThat(it.hasNext(), is(true));
