81aead9440fcf642cf6d3c6e524fb78bd23c3275
Sergey Baikov <rayleonard9@gmail.com>
2019-03-14 09:48:34 +0300

    5.2.2. Реализовать Store<T extends Base>[#118879]

--- chapter_1/src/main/java/ru/job4j/generics/AbstractStore.java
+++ chapter_1/src/main/java/ru/job4j/generics/AbstractStore.java
@@ -2,4 +2,14 @@ package ru.job4j.generics;
 
 public abstract class AbstractStore<T> implements Store{
 
+    public abstract void add(User model);
+
+    public abstract boolean replace(String id, User model);
+
+    public abstract boolean delete(String id);
+
+    public abstract User findById(String id);
+
+    public abstract int indexOf(String id);
+
 }
