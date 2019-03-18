af491c4df15ce5166f933870a5ce98eab06c0651
Sergey Baikov <rayleonard9@gmail.com>
2019-03-18 13:02:15 +0300

    1. Реализовать коллекцию Set на массиве[#118882]

---
+++ chapter_1/src/main/java/ru/job4j/set/SimpleSet.java
@@ -0,0 +1,35 @@
+package ru.job4j.set;
+
+import ru.job4j.list.DinamicArray;
+
+
+public class SimpleSet<E> {
+
+    private DinamicArray<E> set;
+
+    public SimpleSet(int cells) {
+        set = new DinamicArray<>(cells);
+    }
+
+    public boolean duplicate(E model) {
+        boolean result = false;
+        for (E element: set
+             ) {
+            if (element.equals(model)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public void add(E model) {
+        if (!duplicate(model)) {
+            set.add(model);
+        }
+    }
+
+    public E get(int index) {
+       return set.get(index);
+    }
+}
---
+++ chapter_1/src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.set;
+
+
+import org.hamcrest.core.IsNull;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+
+public class SimpleSetTest {
+
+    private SimpleSet<Integer> set;
+
+    @Before
+    public void beforeTest() {
+        set = new SimpleSet<>(2);
+        set.add(1);
+        set.add(1);
+    }
+
+    @Test
+    public void whenAddOneAndOneThenOne() {
+        assertThat(set.get(0), is(1));
+        assertThat(set.get(1), is(IsNull.nullValue()));
+    }
+
+}
