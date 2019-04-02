80f40d2bdff0a3788b825dc11409476617782123
Sergey Baikov <rayleonard9@gmail.com>
2019-04-02 14:41:37 +0300

    Статистику по коллекции.[#118861]

--- chapter_1/src/main/java/ru/job4j/list/Analize.java
+++ chapter_1/src/main/java/ru/job4j/list/Analize.java
@@ -1,24 +1,62 @@
 package ru.job4j.list;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Analize {
 
     public Info diff(List<User> previous, List<User> current) {
         Info result = new Info(0, 0, 0);
-        int deference = previous.size() - current.size();
-        if (deference == 0) {
-            int counter = 0;
-            for (int i = 0; i < previous.size(); i++) {
-                if (!previous.get(i).equals(current.get(i))) {
-                    counter++;
+        List<User> first = previous;
+        List<User> second = current;
+        result.changed = changed(first, second);
+        result.added = added(first, second) - changed(first, second);
+        result.deleted = deleted(first, second) - changed(first, second);
+
+        return result;
+    }
+
+    private int added(List<User> previous, List<User> current) {
+        int result = 0;
+        for (User user : current) {
+            if (!previous.contains(user)) {
+                result ++;
+            }
+        }
+        return result;
+    }
+
+    private int deleted(List<User> previous, List<User> current) {
+        int result = 0;
+        for (User user : previous) {
+            if (!current.contains(user)) {
+                result ++;
+            }
+        }
+        return result;
+    }
+
+    private int changed(List<User> previous, List<User> current) {
+        int result = 0;
+        User temp = null;
+        for (User user : previous) {
+            temp = findById(user.getId(), current);
+            if (temp != null ) {
+                if (!user.getName().equals(temp.getName())) {
+                    result++;
                 }
             }
-            result.changed = counter;
-        } else if (deference < 0){
-            result.added = Math.abs(deference);
-        } else {
-            result.deleted = deference;
+        }
+        return result;
+    }
+
+    private User findById(int id, List<User> list) {
+        User result = null;
+        for (User user : list) {
+            if (user.getId() == id) {
+                result = user;
+                break;
+            }
         }
         return result;
     }
@@ -30,6 +68,28 @@ public class Analize {
         public User(int id, String name) {
             this.id = id;
             this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return id == user.id &&
+                    Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
         }
     }
 
--- chapter_1/src/test/java/ru/job4j/list/AnalizeTest.java
+++ chapter_1/src/test/java/ru/job4j/list/AnalizeTest.java
@@ -55,4 +55,24 @@ public class AnalizeTest {
         assertThat(result.getChanged(), is(1));
         assertThat(result.getDeleted(), is(0));
     }
+
+    @Test
+    public void whenOneUserWasChangedAndOneAddedThenAdded1Changed1() {
+        Analize test = new Analize();
+        Analize.User bob = new Analize.User(1, "Bob");
+        Analize.User tom = new Analize.User(2, "Tom");
+        Analize.User sam = new Analize.User(2, "Sam");
+        Analize.User dan = new Analize.User(4, "Dan");
+        List<Analize.User> previous = new ArrayList<>();
+        previous.add(bob);
+        previous.add(tom);
+        List<Analize.User> current = new ArrayList<>();
+        current.add(bob);
+        current.add(sam);
+        current.add(dan);
+        Analize.Info result = test.diff(previous, current);
+        assertThat(result.getAdded(), is(1));
+        assertThat(result.getChanged(), is(1));
+        assertThat(result.getDeleted(), is(0));
+    }
 }
