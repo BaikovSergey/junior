2b068a522b7df19647316884817fe48672d6794c
Sergey Baikov <rayleonard9@gmail.com>
2019-04-04 16:23:11 +0300

    2. Удаление запрещенных слов[#118905]

--- Input-Output/src/main/java/inputoutput/Analizy.java
+++ Input-Output/src/main/java/inputoutput/Analizy.java
@@ -1,11 +1,21 @@
 package inputoutput;
 
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.StringJoiner;
 
 public class Analizy {
     public void unavailable(String source, String target) {
+        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
+            read.lines().forEach(line -> {
+                String[] code = line.split(" ", 1);
+                String[] timeInterval = new String[2];
+                if (code[0].contains("400") || code[0].contains("500")) {
 
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
