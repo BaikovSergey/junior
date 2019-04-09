9ca97f1fabce3cb46ace07f30bb2ac840b0a0108
Baikov Sergey <rayleonard9@gmail.com>
2019-04-09 21:48:34 +0300

    4. Архивировать проект. [#118906]

--- Input-Output/src/main/java/ru/job4j/inputoutput/Zip.java
+++ Input-Output/src/main/java/ru/job4j/inputoutput/Zip.java
@@ -1,7 +1,32 @@
 package ru.job4j.inputoutput;
 
 public class Zip {
+
+    private String directory(String dir) {
+        String result = null;
+        if (dir != null) {
+            result = dir.substring(dir.indexOf("-d"), dir.indexOf("-e"));
+        }
+        return result;
+    }
+
+    private String exclude(String ex) {
+        String result = null;
+        return result;
+    }
+
+    private String output(String out) {
+        String result = null;
+        return result;
+    }
+
     public static void main(String[] args) {
+        Zip zip = new Zip();
+        StringBuilder parameters = new StringBuilder();
+        for (String s : args) {
+            parameters.append(s);
+        }
+        System.out.println(zip.directory(parameters.toString()));
 
     }
 }
