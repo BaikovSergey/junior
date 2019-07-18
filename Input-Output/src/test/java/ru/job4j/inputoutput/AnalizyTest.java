6738cb4bcf315dce15fc7a1674de5279adc77596
Sergey Baikov <rayleonard9@gmail.com>
2019-07-18 12:23:26 +0300

    Refactor

--- Input-Output/src/test/java/ru/job4j/inputoutput/AnalizyTest.java
+++ Input-Output/src/test/java/ru/job4j/inputoutput/AnalizyTest.java
@@ -1,46 +1,56 @@
 package ru.job4j.inputoutput;
 
 
+import org.junit.Before;
 import org.junit.Test;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class AnalizyTest {
 
-    @Test
-    public void whenLoadConfigFileThenLoadConfigToHashMap() throws IOException {
-        File serverlog = data(
-                String.valueOf(System.currentTimeMillis()),
-                "200 10:56:01"
-                + "200 10:57:01"
-                + "400 10:58:01"
-                + "200 10:59:01"
-                + "500 11:01:02"
-                + "200 11:02:02"
-        );
+    final File tempDirectory = new File(System.getProperty("java.io.tmpdir"));
 
-        File result = data(
-                String.valueOf(System.currentTimeMillis()),
-                ""
-        );
-        Analizy analizy = new Analizy();
-        analizy.unavailable(serverlog.getAbsolutePath(), result.getAbsolutePath());
-        serverlog.deleteOnExit();
+    @Before
+    public void preaper() throws IOException{
+        File unavailable = new File(tempDirectory + File.separator + "unavailable.txt");
+        unavailable.createNewFile();
+        FileWriter writer = new FileWriter(unavailable);
+        writer.write("200 10:56:01");
+        writer.write(System.lineSeparator());
+        writer.write("500 10:57:01");
+        writer.write(System.lineSeparator());
+        writer.write("400 10:58:01");
+        writer.write(System.lineSeparator());
+        writer.write("200 10:59:01");
+        writer.write(System.lineSeparator());
+        writer.write("500 11:01:02");
+        writer.write(System.lineSeparator());
+        writer.write("200 11:02:02");
+        writer.write(System.lineSeparator());
+        writer.close();
+        unavailable.deleteOnExit();
+        File result = new File(tempDirectory + File.separator + "result.txt");
+        result.createNewFile();
         result.deleteOnExit();
     }
 
-    private File data(String file, String... properties) throws IOException {
-        File path = new File(
-                System.getProperty("java.io.tmpdir")
-                        + System.clearProperty("file.separator")
-                        + file
-        );
-        if (!path.createNewFile()) {
-            throw new IllegalStateException(String.format("File could not created %s", path.getAbsoluteFile()));
+    @Test
+    public void whenLoadLogFileThenFileWithServerDownTime() {
+        List<String> result = new ArrayList<>();
+        Analizy test = new Analizy();
+        test.unavailable(tempDirectory + File.separator + "unavailable.txt",
+                tempDirectory + File.separator + "result.txt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(tempDirectory + File.separator + "result.txt"))){
+            reader.lines().forEach(result::add);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        try (final PrintWriter store = new PrintWriter(path)) {
-            Stream.of(properties).forEach(store::println);
-        }
-        return path;
+        assertThat(result.get(0), is("10:57:01;10:59:01"));
+        assertThat(result.get(1), is("11:01:02;11:02:02"));
     }
-
 }
