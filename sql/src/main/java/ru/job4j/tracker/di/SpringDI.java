e37ce62113608d7086ac9d369b122116445e9fe0
Baikov Sergey <rayleonard9@gmail.com>
2020-10-22 15:43:33 +0300

    DI Spring scan

--- sql/src/main/java/ru/job4j/tracker/di/SpringDI.java
+++ sql/src/main/java/ru/job4j/tracker/di/SpringDI.java
@@ -1,14 +1,11 @@
 package ru.job4j.tracker.di;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.job4j.tracker.ConsoleInput;
 
 public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(Store.class);
-        context.register(ConsoleInput.class);
-        context.register(StartUIdi.class);
+        context.scan("ru.job4j.di", "ru.job4j.tracker");
         context.refresh();
         StartUIdi ui = context.getBean(StartUIdi.class);
         ui.add("Petr Arsentev");
